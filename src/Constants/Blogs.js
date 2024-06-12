5f4d05cfa3321e5f7b7bba0dfceb38558f2af047
theHeliE <129796088+theHeliE@users.noreply.github.com>
2024-06-12 17:05:31 +0300

    Home v2

---
+++ src/Constants/Blogs.js
@@ -0,0 +1,7 @@
+const intro =
+  "This is my very humble blog where I will be sharing my projects, major events, my hobbies, my acheivements in video games and maybe some of my opinions on certain topics :).";
+const fullIntro =
+  intro +
+  "Please understand that this is purely my opinion and I may be wrong, it's human nature, but feel free to contact me if you want to discuss anything with me or if you have any questions and thank you!";
+
+export { intro, fullIntro };
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,7 @@ import { shortBio } from "../Constants/Bio.js";
 import Button from "@material-ui/core/Button";
 import Cards from "../Components/Cards.js";
 import announcements from "../Constants/Announcements.js";
+import { intro } from "../Constants/Blogs.js";
 function Home() {
   return (
     <>
@@ -16,18 +17,23 @@ function Home() {
         <h1 className="subtitle tungsten-text">Latest</h1>
         <div className="announcements-container">
           {announcements.map((announcement, index) => (
-            <div className="announcement" key={index}>
+            <a className="announcement" key={index} href="#e">
               <Cards
                 img={announcement.img}
                 title={announcement.title}
                 desc={announcement.description}
               />
-            </div>
+            </a>
           ))}
         </div>
       </section>
       <section className="section-1">
-        <h1 className="home-title tungsten-text">TheHazem</h1>
+        <h1
+          className="home-title tungsten-text hover-title"
+          data-text="TheHazem"
+        >
+          TheHazem
+        </h1>
         <div className="sub-section">
           <p className="tungsten-medium paragraph-text">{shortBio}</p>
         </div>
@@ -36,6 +42,42 @@ function Home() {
           style={{ color: "black", marginLeft: "5%", marginBottom: "0" }}
         >
           <p className="tungsten-medium buttons">READ MORE</p>
+        </Button>
+      </section>
+      <section className="section-2">
+        <h1 className="home-section tungsten-text" data-text="TheHazem">
+          TheHazem Blog
+        </h1>
+        <div className="sub-section">
+          <p className="tungsten-medium paragraph-text2">{intro}</p>
+        </div>
+        <Button
+          variant="contained"
+          style={{ color: "black", marginLeft: "5%", marginBottom: "0" }}
+        >
+          <p className="tungsten-medium buttons">CHECK OUT</p>
+        </Button>
+      </section>
+      <section className="section-3">
+        <h1
+          className="home-title tungsten-text hover-title"
+          data-text="TheHazem"
+        >
+          Projects
+        </h1>
+        <div className="sub-section">
+          <p className="tungsten-medium paragraph-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <Button
+          variant="contained"
+          style={{ color: "black", marginLeft: "5%", marginBottom: "0" }}
+        >
+          <p className="tungsten-medium buttons">CHECK PROJECTS</p>
         </Button>
       </section>
     </>
